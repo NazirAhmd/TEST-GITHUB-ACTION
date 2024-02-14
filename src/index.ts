ba8afa2c603aa01f8d564745d689c1c35fa51115
Nazir Ahmad <nazirahmad@microsoft.com>
2024-02-14 12:09:15 +0530

    created sample github-action

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log("Hello World!");
+import * as core from '@actions/core'
+
+async function run(isLocal: boolean) {
+    console.log("test-github-action is executing......");
+
+    let waitInMs: string = core.getInput('milliseconds');
+
+    if (isLocal)
+        waitInMs = '50000';
+
+    console.log(`Waiting for ${waitInMs}ms`);
+    await wait(parseInt(waitInMs));
+    console.log('Wait is over.');
+
+    core.setOutput('time', new Date().toTimeString())
+
+    function wait(waitInMs: number): Promise<string> {
+        return new Promise<string>(resolve => {
+            if (isNaN(waitInMs)) {
+                throw new Error('waitInMs is not a number.');
+            }
+            setTimeout(() => resolve('Done!'), waitInMs);
+        });
+    }
+}
+
+run(false);
+
+// run(true);
